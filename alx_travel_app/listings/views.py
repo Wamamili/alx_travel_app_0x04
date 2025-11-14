5b69477126e1093bec8c7cefb0210dfb53fb410f
wamamili <wamamiliclean@gmail.com>
2025-11-14 12:04:22 +0300

    commit

--- alx_travel_app/listings/views.py
+++ alx_travel_app/listings/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Listing, Booking
+from .serializers import ListingSerializer, BookingSerializer
 
-# Create your views here.
+class ListingViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing listing instances.
+    """
+    queryset = Listing.objects.all().order_by('-created_at')
+    serializer_class = ListingSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing booking instances.
+    """
+    queryset = Booking.objects.all().order_by('-booked_at')
+    serializer_class = BookingSerializer
